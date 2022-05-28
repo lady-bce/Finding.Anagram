e7444fce65913abaa487605fb7988e4a34d66481
lady-bce <105195567+lady-bce@users.noreply.github.com>
2022-05-28 22:15:08 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(str1, str2):
+    # [assignment] Add your code here
+    str1 = "elbow"
+    str2 = "below"
+    str1_anagram = sorted(str1)
+    str2_anagram = sorted(str2)
+
+    if str1_anagram == str2_anagram:
+        return True
+    else:
+        return False
+print(find_anagram("elbow", "below"))       
+
